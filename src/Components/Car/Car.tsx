e8db7f4d471d7f35ffc47925e834c03b012545d8
JulianFullStack <contacto3244003011@gamil.com>
2025-02-07 21:52:09 -0500

    up

--- src/Components/Car/Car.tsx
+++ src/Components/Car/Car.tsx
@@ -1,18 +1,31 @@
-import { Box,Image } from "@chakra-ui/react";
-import { backgroundAboutNavbar } from "../../Constants/PublicStyle.ts";
+import {Box, Center, grid, Select} from "@chakra-ui/react"
+import { menu } from "../../Constants/PublicConst.ts";
+import NavBar from '../NavBarBasic/NavBar.tsx'
 import React  from 'react';
-function Car() { 
-  return (    
-    <>
-      <Box sx={backgroundAboutNavbar}> 
-        <Box display= "grid" gridTemplateColumns= "60% 40%">
-          
+const Car = () => {
+
+    return  (
+        <Box>            
+            <NavBar/>
+            <Box margin= "2vh" h= "82vh" textAlign={"left"} >
+                  <Box border={"solid silver 1px"} borderRadius={"5px"} padding={"5px"} margin = "0 auto" marginBottom={"15px"} maxW={"700px"} >
+                    <Box display={"grid"} gridTemplateColumns={"90% 10%"}>                      
+                    <Box>Camara de seguridad HD de 4 mp y WIFI 2.4 ($14.254.000)</Box>
+                    <Box textAlign={"center"} color={"#4369ca"} fontWeight={"bold"}>X</Box>
+                    </Box>
+                    <Box display={"flex"}>
+                      <Select bg={"whithe"} color={"#4369ca"} textAlign={"right"} placeholder="Por 100" h={"30px"}border ={"none"} focusBorderColor="Transparent" maxW={"45%"}>
+                        <option value="200">Por 200</option>
+                        <option value="300">Por 300</option>
+                      </Select>
+                      <Select w={"45%"} bg={"whithe"} color={"#4369ca"} textAlign={"right"} placeholder="Rojo 125 GB" h={"30px"} border ={"none"}focusBorderColor="Transparent">
+                        <option value="2">Rojo 256 gb</option>
+                        <option value="3">Verde 125 GB</option>
+                      </Select>
+                    </Box>
+                  </Box>
+            </Box>
         </Box>
-      </Box>    
-      <Box>
-        Lista
-      </Box>
-    </>
-  );
+    )
 }
 export default Car;
